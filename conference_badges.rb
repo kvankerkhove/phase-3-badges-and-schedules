ff425cdc0d9b9aef7c67c0bedb9f526d7057fa3a
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-05-16 16:35:36 -0400

    commit tree 06d3b39e8820f3fb117543c484ae4f40a058efe9

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array_of_names
+    array_of_names.map{|name| badge_maker(name)}
+end
+
+def assign_rooms array_of_speakers
+    array_of_speakers.map.with_index{|name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+end
+
+def printer attendees
+    batch_badge_creator(attendees).each{|badge| puts badge}
+    assign_rooms(attendees).each{|room_assignment| puts room_assignment}
+end
+
+
+
+
